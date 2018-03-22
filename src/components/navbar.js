a94d66f24b0dd05cdda1511b7e120c46889458b1
Davia DeNisco <davia622@gmail.com>
2018-03-22 14:41:42 -0400

    Add navbar
    Added navbar.js, navbar.css, populated both those files with code to mirror Git Duel. Also updated index.html with a div (id="navbar") and updated the index.js file to import, and render, NavBar.

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./navbar.css";
+
+const NavBar = () =>
+
+  <div className="jumbotron jumbotron-fluid bg-warning">
+    <div className="container">
+      <h1 className="display-3">Git Duel</h1>
+      <p className="lead">Compare GitHub accounts with your friends!</p>
+    </div>
+  </div>
+
+export default NavBar;
